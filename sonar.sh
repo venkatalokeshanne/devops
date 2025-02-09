fc87044871a62e96bbfaaef8825f8818468061b9
Naveen <Venkata Lokesh Anne>
2025-02-09 08:56:53 +0100

    Sonar fix

--- sonar.sh
+++ sonar.sh
@@ -1,4 +1,4 @@
 # Description: Script to run sonarqube container
 
-docker run -d --name sonarqube -p 9000:9000 sonarqube:latest       
+docker run -d --name sonarqube -p 9000:9000 sonarqube:lts      
 
